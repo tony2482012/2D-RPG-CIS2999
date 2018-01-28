956c5d396a8fa2e1e7a08a8016569380ebe123b4
tony2482012 <anthonyyono55@me.com>
2018-01-28 15:41:27 -0500

    work

---
+++ 2D RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Anthony Yono
+
+public class Player : Character 
+{
+
+	// Update is called once per frame
+	protected override void Update () 
+	{
+		GetInput();
+		base.Update ();
+	}
+		
+	private void GetInput()
+	{
+		direction = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W)) 
+		{
+			direction += Vector2.up;
+		}
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			direction += Vector2.left;
+		}
+		if (Input.GetKey(KeyCode.S)) 
+		{
+			direction += Vector2.down;
+		}
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			direction += Vector2.right;
+		}
+	}
+}
