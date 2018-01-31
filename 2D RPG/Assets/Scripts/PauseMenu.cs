b072479ea0d5b156c7e768bb5a85a33107a448e8
Selena <shaidar2@oakland.edu>
2018-01-31 15:54:43 -0500

    Main/Pause Menu

---
+++ 2D RPG/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public string MainMenu;
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+
+	// Update is called once per frame
+	void Update () {
+		
+        if (isPaused) {
+            pauseMenuCanvas.SetActive(true);
+            //Time.timeScale = 0f;
+        } else {
+            pauseMenuCanvas.SetActive(false);
+            //Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPaused = !isPaused;
+        }
+
+	}
+
+    public void Resume () {
+
+        isPaused = false;
+        
+    }
+
+    public void Save() {
+        
+    }
+
+    public void Quit() {
+        
+        SceneManager.LoadScene(MainMenu);
+        //SceneManager.LoadScene("CombatTestScene");
+        //SceneManager.SetActiveScene("TitleMenu");
+
+    }
+}
