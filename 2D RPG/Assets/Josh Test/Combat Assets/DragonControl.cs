10734ba6f5ccbda2613f623896117b1390566611
JJBunnell <JJBunnell@oakland.edu>
2018-01-31 14:35:12 -0500

    DragonAdd
    Added Dragon idle and attack animations

---
+++ 2D RPG/Assets/Josh Test/Combat Assets/DragonControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonControl : MonoBehaviour {
+	public float snakeHP = 50;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Battleflow.whichTurn == 4) {
+			GetComponent<Animator> ().SetTrigger ("DragonSmash1");
+			GetComponent<Transform>().position = new Vector2 (4.0f, -1.79f);
+			Battleflow.whichTurn = 1;
+		}
+	}
+
+	void returnDragon(){
+		GetComponent<Transform> ().position = new Vector2 (-4.18f, 1.45f );
+	}
+}
