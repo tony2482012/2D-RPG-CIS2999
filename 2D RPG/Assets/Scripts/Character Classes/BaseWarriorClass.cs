6a5e212909948c718f275981dfa41a988ea42d8c
tony2482012 <anthonyyono55@me.com>
2018-02-07 19:42:00 -0500

    apy

---
+++ 2D RPG/Assets/Scripts/Character Classes/BaseWarriorClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWarriorClass : BaseCharacterClass {
+	public BaseWarriorClass(){
+		ChacterClassName = "Warrior";
+		CharacterClassDescription = "A strong Fighter";
+		Stamina = 8;
+		Endurance = 10;
+		Strength = 12;
+		Intellect = 4;
+	}
+
+}
