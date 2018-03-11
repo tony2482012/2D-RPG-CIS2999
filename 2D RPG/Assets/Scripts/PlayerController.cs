40a21630bfc7317427608bf059eafa30cfbe7b39
tony2482012 <35173103+tony2482012@users.noreply.github.com>
2018-03-11 14:04:17 -0400

    Anthony

--- 2D RPG/Assets/Scripts/PlayerController.cs
+++ 2D RPG/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
     private Animator anim;
+    private bool PlayerMoving;
+    private Vector2 lastMove;
 
 	// Anthony Yono
 	void Start ()
@@ -17,18 +19,26 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        PlayerMoving = false;
         //Move left and right
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         //Move up and down
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            PlayerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
+
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-
+        anim.SetBool("PlayerMoving", PlayerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
