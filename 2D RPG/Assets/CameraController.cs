deafa2f25e431266e57ea4dd874a2e49275970b0
Cody Yandura <35173272+cyandura@users.noreply.github.com>
2018-03-28 14:26:00 -0400

    commit

---
+++ 2D RPG/Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	public Camera myCamera;
+	public float fieldOfView;
+
+	void Awake(){
+		
+		myCamera.fieldOfView = 90.0f;	
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
