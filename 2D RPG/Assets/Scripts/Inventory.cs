49643cd58ba490bf051667ab057428776e50a161
Cody Yandura <35173272+cyandura@users.noreply.github.com>
2018-04-09 18:18:28 -0400

    c

---
+++ 2D RPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion 
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallBack;
+
+    public List<Item> items = new List<Item>();
+
+    public int space = 4;
+
+
+    public bool add( Item item)
+    {
+        if (items.Count >= space)
+            return false;
+       
+        items.Add(item);
+
+        if(onItemChangedCallBack != null)
+            onItemChangedCallBack.Invoke();
+
+        return true;
+    }
+
+    public void remove (Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallBack != null)
+            onItemChangedCallBack.Invoke();
+    }
+}
---
+++ 2D RPG/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class InventorySlot : MonoBehaviour {
+    public Image pic;
+    Item item;
+
+    public void addItem(Item newItem)
+    {
+        item = newItem;
+        
+        pic.sprite = item.icon;
+        
+        pic.enabled = true;
+
+    }
+
+    public void clearSlot()
+    {
+        item = null;
+
+        pic.sprite = null;
+
+        pic.enabled = false;
+
+    }
+
+}
