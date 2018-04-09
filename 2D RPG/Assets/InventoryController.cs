9950988a0032901d0bec010cbf06687dcc37f00b
Cody Yandura <35173272+cyandura@users.noreply.github.com>
2018-04-09 19:06:51 -0400

    r

---
+++ 2D RPG/Assets/InventoryController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+	InventoryController inventory;
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
