a7ade6f313325cc004571985561dce4ec83ce0b4
JJBunnell <JJBunnell@oakland.edu>
2018-04-09 20:29:53 -0400

    Commit

--- 2D RPG/Assets/Scripts/LoadNewArea.cs
+++ 2D RPG/Assets/Scripts/LoadNewArea.cs
@@ -26,16 +26,12 @@ public class LoadNewArea : MonoBehaviour {
         }
       
     }
-<<<<<<< HEAD
+
 
 	void OnCollisionEnter2D(Collision2D collision){
 		GetComponent<Collider> ().enabled = false;
 	}
-=======
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        GetComponent<Collider>().enabled = false;
-    }
 
->>>>>>> c0a96a6a72462c51280bbd60b8e726e9f2bfe54f
+
+
 }
